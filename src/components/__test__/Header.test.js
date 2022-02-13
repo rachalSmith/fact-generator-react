4124ed04eae42c6f460bbfbbbb7e55c8f4584545
rachalsmith <66318063+rachalSmith@users.noreply.github.com>
2022-02-13 11:07:09 +0000

    Delete src/components/__test__ directory

--- src/components/__test__/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import Header from '../Header'
-
-it('should have a header of animal adaptations', () => {
-  render(<Header />);
-  const headerElement = screen.getByRole('heading');
-  expect(headerElement).toBeInTheDocument();
-});
